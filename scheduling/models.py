dc688cf2ac95c5596fdd56f70069c5e08551683d
Nelson <nelson.chamberlain7@gmail.com>
2024-11-14 11:56:08 -0800

    Updated Employee Model for grins and giggles

--- scheduling/models.py
+++ scheduling/models.py
@@ -5,6 +5,8 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
+    department = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
