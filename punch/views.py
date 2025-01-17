3b23b17600e6728079d7ec43ec5619d5f12a56bc
Luiz Arjon <luizarjon@gmail.com>
2025-01-16 16:51:41 -0800

    Removed permission

--- punch/views.py
+++ punch/views.py
@@ -4,6 +4,6 @@ from django.utils.timezone import now
 from api.models import User, Punch
 from administration.views import permission_required
 
-@permission_required("Punch Management")  # Pass the required `function_name` argument
+# @permission_required("Punch Management") 
 def punch_landing(request):
     return render(request, 'punch/punch_landing.html')
